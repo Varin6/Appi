a516ab8916e950bd99b27a7fe403aa5556c9caba
Varin6 <varinator@gmail.com>
2019-01-28 22:31:00 +0000

    simple api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Note;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,32 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('notes', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Note::all();
+});
+
+Route::get('notes/{id}', function($id) {
+    return Note::find($id);
+});
+
+Route::post('notes', function(Request $request) {
+    return Note::create($request->all);
+});
+
+Route::put('notes/{id}', function(Request $request, $id) {
+    $article = Note::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('notes/{id}', function($id) {
+    Note::find($id)->delete();
+
+    return 204;
 });
