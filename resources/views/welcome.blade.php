3117d5537082f25197838583ab8ad89f48b64eff
Varin6 <varinator@gmail.com>
2019-01-28 20:03:36 +0000

    docker, docker-composer files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Appi
                 </div>
 
                 <div class="links">
